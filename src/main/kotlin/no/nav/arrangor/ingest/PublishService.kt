fc75ef8c9978ea4655f692cdc6b0d5eab4dd1dc0
Magnus Evensberget <magnus.evensberget@nav.no>
2023-06-19 12:56:29 +0200

    985: Skrudde av publisering av ansatte

--- src/main/kotlin/no/nav/arrangor/ingest/PublishService.kt
+++ src/main/kotlin/no/nav/arrangor/ingest/PublishService.kt
@@ -6,13 +6,15 @@ import no.nav.arrangor.domain.Arrangor
 import no.nav.arrangor.dto.ArrangorDto
 import no.nav.arrangor.utils.JsonUtils
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Component
 
 @Component
 class PublishService(
 	private val template: KafkaTemplate<String, String>,
-	private val metricsService: MetricsService
+	private val metricsService: MetricsService,
+	@Value("\${publish.ansatt.enabled}") private val publishAnsatte: Boolean
 ) {
 
 	private val logger = LoggerFactory.getLogger(javaClass)
@@ -24,9 +26,13 @@ class PublishService(
 	}
 
 	fun publishAnsatt(ansatt: Ansatt) {
-		template.send(ANSATT_TOPIC, ansatt.id.toString(), JsonUtils.toJson(ansatt))
-			.also { metricsService.incPubliserteAnsatte() }
-			.also { logger.info("Publiserte ansatt med id ${ansatt.id}") }
+		if (publishAnsatte) {
+			template.send(ANSATT_TOPIC, JsonUtils.toJson(ansatt))
+				.also { metricsService.incPubliserteAnsatte() }
+				.also { logger.info("Publiserte ansatt med id ${ansatt.id}") }
+		} else {
+			logger.info("Publisering av meldinger er skrudd av, publiserer ikke ansatt med id ${ansatt.id}")
+		}
 	}
 
 	private fun Arrangor.toDto(): ArrangorDto = ArrangorDto(
