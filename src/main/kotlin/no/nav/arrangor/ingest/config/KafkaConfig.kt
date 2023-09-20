145079007e317283a8522e9ee90d5a235c6f4476
Tia Firing <tia.firing@nav.no>
2023-09-20 14:29:02 +0200

    egen listener for å lese deltakertopic fra start

--- src/main/kotlin/no/nav/arrangor/ingest/config/KafkaConfig.kt
+++ src/main/kotlin/no/nav/arrangor/ingest/config/KafkaConfig.kt
@@ -69,6 +69,27 @@ class KafkaConfig(
 	}
 
 	@Bean
+	fun kafkaListenerContainerFactoryDeltakerTopic(
+		kafkaErrorHandler: KafkaErrorHandler
+	): ConcurrentKafkaListenerContainerFactory<String, String> {
+		val config = mapOf(
+			ConsumerConfig.GROUP_ID_CONFIG to "amt-arrangor-consumer-3",
+			ConsumerConfig.AUTO_OFFSET_RESET_CONFIG to kafkaAutoOffsetReset,
+			ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG to false,
+			ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG to StringDeserializer::class.java,
+			ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG to StringDeserializer::class.java,
+			ConsumerConfig.MAX_POLL_RECORDS_CONFIG to "1"
+		) + commonConfig()
+		val consumerFactory = DefaultKafkaConsumerFactory<String, String>(config)
+
+		val factory = ConcurrentKafkaListenerContainerFactory<String, String>()
+		factory.consumerFactory = consumerFactory
+		factory.setCommonErrorHandler(kafkaErrorHandler)
+		factory.containerProperties.ackMode = ContainerProperties.AckMode.MANUAL_IMMEDIATE
+		return factory
+	}
+
+	@Bean
 	fun kafkaProducerFactory(): ProducerFactory<String, String> {
 		return DefaultKafkaProducerFactory(commonConfig())
 	}
