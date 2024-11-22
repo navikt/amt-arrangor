74724f9a0461c38b35aea4b10828fb4ccd964044
tiafiring <tiafiring@gmail.com>
2024-11-22 07:18:38 +0100

    oppgraderer til nyeste spring boot

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     val kotlinVersion = "2.0.21"
 
-    id("org.springframework.boot") version "3.3.6"
+    id("org.springframework.boot") version "3.4.0"
     id("io.spring.dependency-management") version "1.1.6"
     id("org.jlleitschuh.gradle.ktlint") version "12.1.1"
     kotlin("jvm") version kotlinVersion
