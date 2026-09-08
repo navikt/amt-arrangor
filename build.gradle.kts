f35e05f32b58cc8fd1e6f1f11495acae8d942bca
Roar Skinderviken <roar.skinderviken@gmail.com>
2026-09-08 13:09:55 +0200

    komet-bau: Fixes for CVE-2026-59949 og CVE-2026-65182

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,19 @@ dependencyManagement {
     }
 }
 
+// midlertidig fix for CVE-2026-65182
+extra["tomcat.version"] = "11.0.25"
+
 dependencies {
+    constraints {
+        implementation("at.yawk.lz4:lz4-java") {
+            version {
+                strictly("1.11.2")
+            }
+            because("Fixes CVE-2026-59949")
+        }
+    }
+
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-validation")
